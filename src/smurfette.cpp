8c4a28e1e89b304d43c843026ae6919feca1a9bd
Eric Mikulin <ericm99@gmail.com>
2019-09-24 23:03:48 -0700

    Finish main loop skeleton

--- src/smurfette.cpp
+++ src/smurfette.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <chrono>
+#include <cstdint>
 #include <thread>
 #include <mutex>
 #include "uastelem.h"
 #include "utils.h"
 
-constexpr int TLEM_THREAD_WAIT = 1000;
-constexpr int MAIN_THREAD_WAIT = 1000;
+constexpr int TLEM_THREAD_WAIT_MS = 1000;
+constexpr int MAIN_THREAD_WAIT_MS = 1000;
+constexpr int MAX_TIMESTAMP_DIFF_MS = 100;
 
 std::mutex aircraftTelemMutex;
 UasTelem aircraftSkylinkTelem;
@@ -16,12 +18,18 @@ UasTelem groundSkylinkTelem;
 
 void telemetryGatherThread(std::mutex* mutexPtr, UasTelem* skylinkTelemPtr) {
     while(true) {
+        // Get telem from serial socket
+
         mutexPtr->lock();
         skylinkTelemPtr->update(0.0, 0.0, 0.0);
         mutexPtr->unlock();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(TLEM_THREAD_WAIT));
+        std::this_thread::sleep_for(std::chrono::milliseconds(TLEM_THREAD_WAIT_MS));
     }
+}
+
+bool isTelemTimestampValid(int64_t timestamp, int64_t telemTimestamp) {
+    return (timestamp - telemTimestamp <= MAX_TIMESTAMP_DIFF_MS) ? true : false;
 }
 
 int main() {
@@ -29,9 +37,31 @@ int main() {
     std::thread groundTelemThread (telemetryGatherThread, &groundTelemMutex, &groundSkylinkTelem);
 
     while(true) {
-        std::cout << "Hello World" << std::endl;
+        UasTelem uasTelem;
+        bool validTelem = false;
+        int64_t timestamp = utils::timeSinceEpochMillisec();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(MAIN_THREAD_WAIT));
+        groundTelemMutex.lock();
+        if (!validTelem && isTelemTimestampValid(timestamp, groundSkylinkTelem.timestamp())) {
+            uasTelem = groundSkylinkTelem;
+            validTelem = true;
+        }
+        groundTelemMutex.unlock();
+
+        aircraftTelemMutex.lock();
+        if (!validTelem && isTelemTimestampValid(timestamp, aircraftSkylinkTelem.timestamp())) {
+            uasTelem = aircraftSkylinkTelem;
+            validTelem = true;
+        }
+        aircraftTelemMutex.unlock();
+
+        if (validTelem) {
+            // Place telem into database
+        } else {
+            std::cout << "No valid TELEM found" << std::endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(MAIN_THREAD_WAIT_MS));
     }
 
     aircraftTelemThread.join();
